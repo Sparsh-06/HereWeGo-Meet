97453e4e33c0e79a45b2c8cbb4c0c4c9f5f5195e
Sparsh Sharma <133492571+Sparsh-06@users.noreply.github.com>
2023-07-18 21:16:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ let createPeerConnection = async (MemberId)=>{
     document.getElementById('user-1').classList.add('smallFrame')
 
     if(!localStream){
-        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+        localStream = await navigator.mediaDevices.getUserMedia(constraints)
         document.getElementById('user-1').srcObject = localStream
     }
 
